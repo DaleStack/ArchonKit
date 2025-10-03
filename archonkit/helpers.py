ba046f96536be4872f02d6f87a10b5fa6005e02f
DaleStack <211448639+DaleStack@users.noreply.github.com>
2025-10-04 02:13:43 +0800

    fix: set debug in the .env

--- archonkit/helpers.py
+++ archonkit/helpers.py
@@ -63,7 +63,7 @@ load_dotenv()
 class Settings(BaseSettings):
     # DATABASE_URL: str = os.getenv("DATABASE_URL")
     SECRET_KEY: str = os.getenv("SECRET_KEY")
-    DEBUG = True
+    DEBUG: str = os.getenv("DEBUG")
     
     class Config:
         env_file = ".env"
